9dfb66470087179872a3b9b32b434bc0d66b9f2c
Rapheephat85 <rapheephathanwong85@gmail.com>
2026-07-07 15:26:44 +0700

    Add files via upload

---
+++ test01.py
@@ -0,0 +1,20 @@
+#print("Hello, World!")
+
+#print(5+18//2-25%4)
+
+
+
+
+# age = input("Enter your age: ")
+# age = int(age)
+# height = input("Enter your height in meters: ")
+# height = float(height)
+
+# print("Your are " + str(age) + " years old and " + str(height) + " Feet tall.")
+
+
+weight = float(input("Enter your weight in kilograms: "))
+height = float(input("Enter your height in meters: "))
+bmi = weight / (height * height)
+print(f"Your BMI is: {bmi:.2f}")
+
